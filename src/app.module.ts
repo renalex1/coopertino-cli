51119ba5b3a7f81bf1630eff686d74db940f26f8
renalex <aleksandr@codetime.am>
2024-10-12 10:02:20 +0400

    add i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { AcceptLanguageResolver, I18nModule } from 'nestjs-i18n';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [{ use: AcceptLanguageResolver, options: ['lang', 'l'] }],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
