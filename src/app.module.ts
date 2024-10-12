18dd89bc0c2e9f1255a3561c3647101c006607a1
renalex <aleksandr@codetime.am>
2024-10-12 14:57:03 +0400

    Change translated file name

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,13 @@ import { AcceptLanguageResolver, I18nModule } from 'nestjs-i18n';
 import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+console.log(path.join(__dirname, './i18n/'));
 @Module({
   imports: [
     I18nModule.forRoot({
       fallbackLanguage: 'en',
       loaderOptions: {
-        path: path.join(__dirname, '/i18n/'),
+        path: path.join(__dirname, './i18n/'),
         watch: true,
       },
       resolvers: [{ use: AcceptLanguageResolver, options: ['lang', 'l'] }],
